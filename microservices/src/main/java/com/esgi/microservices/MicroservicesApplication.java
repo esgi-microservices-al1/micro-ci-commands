54e0f73642cd7f048c4f9e5846f6565dc8cfccb0
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-05-25 11:01:53 +0200

    add Swagger for API  and ui

--- microservices/src/main/java/com/esgi/microservices/MicroservicesApplication.java
+++ microservices/src/main/java/com/esgi/microservices/MicroservicesApplication.java
@@ -2,12 +2,13 @@ package com.esgi.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MicroservicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesApplication.class, args);
     }
-
 }
