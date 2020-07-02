eb96783b33f0210f165679a01cabc9c08a29a352
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-02 14:24:10 +0200

    add queue in message broker and consumer queue project

--- microservices/src/main/java/com/esgi/microservices/config/RabbitMQConfig.java
+++ microservices/src/main/java/com/esgi/microservices/config/RabbitMQConfig.java
@@ -4,16 +4,16 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @Configuration
-public class RabbitMQConfig {
+public class RabbitMQConfig implements RabbitListenerConfigurer {
 
     @Value("${rabbitmq.exchange}")
     private String exchange;
@@ -24,21 +24,49 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue}")
     private String queueName;
 
+//    @Value("${spring.rabbitmq.host}")
+//    private String host;
+//
+//    @Value("${rabbitmq.routingKey}")
+//    private String routingkey;
+//
+//    @Value("${rabbitmq.queue}")
+//    private String queueName;
+
+//    @Bean
+//    public MessageConverter messageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+
     @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MappingJackson2MessageConverter jackson2Converter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        return converter;
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(messageConverter());
-        return template;
+    public DefaultMessageHandlerMethodFactory myHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(jackson2Converter());
+        return factory;
     }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(myHandlerMethodFactory());
+    }
+
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+//        template.setMessageConverter(messageConverter());
+//        return template;
+//    }
+
 
     @Bean
     Queue queue() {
-        return new Queue(queueName, false, false, false, null);
+        return new Queue(queueName, true, false, false, null);
     }
 
     @Bean
@@ -52,4 +80,13 @@ public class RabbitMQConfig {
     }
 
 
+//    @Bean
+//    public ConnectionFactory connectionFactory() {
+//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+//        connectionFactory.setAddresses("10.0.0.5");
+//        connectionFactory.setPort(40701);
+//        connectionFactory.setUsername("esgi-al1");
+//        connectionFactory.setPassword("g5Y86ck*uQ2&s57");
+//        return connectionFactory;
+//    }
 }
