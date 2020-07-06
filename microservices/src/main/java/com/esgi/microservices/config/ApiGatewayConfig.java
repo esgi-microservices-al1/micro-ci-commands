9cdd6e66d8e3659bfbb65387b50e34ad71dcc502
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-07 00:57:40 +0200

    add consul in prod

--- microservices/src/main/java/com/esgi/microservices/config/ApiGatewayConfig.java
+++ microservices/src/main/java/com/esgi/microservices/config/ApiGatewayConfig.java
@@ -12,9 +12,6 @@ public class ApiGatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/**")
-                        .filters(f -> f.rewritePath("/api/v1/(?<remains>.*)", "/${remains}")
-                                .preserveHostHeader()
-                        )
                         .uri("lb://micro-ci-commands")
                         .id("micro-ci-commands")
                 ).build();
