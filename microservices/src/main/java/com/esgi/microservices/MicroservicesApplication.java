3a90f91c73fb83e381484afdf803e80e8ed9760a
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-07 01:45:04 +0200

    config API-Gateway

--- microservices/src/main/java/com/esgi/microservices/MicroservicesApplication.java
+++ microservices/src/main/java/com/esgi/microservices/MicroservicesApplication.java
@@ -3,10 +3,12 @@ package com.esgi.microservices;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 
-@SpringBootApplication
+@Configuration
 @EnableRabbit
+@SpringBootApplication
 public class MicroservicesApplication {
 
     public static void main(String[] args) {
