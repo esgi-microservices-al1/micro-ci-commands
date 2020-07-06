ff32433c376e0b0364b1eed33a15cae92b06418e
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-07 00:26:13 +0200

    add consul in prod

--- microservices/src/main/java/com/esgi/microservices/config/ApiGatewayConfig.java
+++ microservices/src/main/java/com/esgi/microservices/config/ApiGatewayConfig.java
@@ -1,23 +1,19 @@
 package com.esgi.microservices.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfig {
 
-//    @Bean
-//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route( r-> r.path("/api/v1/commands**")
-//                        .filters(f->f.setResponseHeader("/api/v1/(?<remains>.*)","/${remains}")
-//                                .preserveHostHeader()
-//                        )
-//                        .uri("http://localhost:8899")
-//                )
-////                .route(r -> r.path("/api/v1/**")
-////                        .filters(f -> f.rewritePath("/api/v1/projects/(?<remains>.*)", "/${remains}")                                )
-////                        .uri("lb://microservices")
-////                        .id("microservices"))
-//                .build();
-//    }
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("1", r -> r
+                        .path("/**")
+                        .uri("http://10.0.0.4:8899/"))
+                .build();
+    }
 }
