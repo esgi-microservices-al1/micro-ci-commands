88e83b88958cdff80445e8bb4bf54fb73ba7ad22
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-10 23:27:18 +0200

    add config for consul

--- microservices/src/main/java/com/esgi/microservices/MicroservicesApplication.java
+++ microservices/src/main/java/com/esgi/microservices/MicroservicesApplication.java
@@ -3,9 +3,11 @@ package com.esgi.microservices;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDiscoveryClient
 @EnableRabbit
 @SpringBootApplication
 public class MicroservicesApplication {
